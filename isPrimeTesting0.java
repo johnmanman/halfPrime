51ecfbc5b79ac5c59e6959eb09ad4843d018bb20
johnmanman <57596728+johnmanman@users.noreply.github.com>
2019-12-13 17:07:32 -0500

    Update isPrimeTesting0.java

--- isPrimeTesting0.java
+++ isPrimeTesting0.java
@@ -2,55 +2,50 @@ package practice;
 
 import java.util.Scanner;
 
-public class PrimeClass3 {
-	
+public class PrimeClass {
+
 	boolean primeTesting(int value) {
 		int count = 0;
 		int root = (int) Math.sqrt(value);
 		boolean fact = true;
 
-		if (value < = 0) {
-			System.out.println("Try another positive Number please");
+		if (value <= 0) {
+			System.out.println("please give a positive number");
 			return false;
 		}
 
 		if (value % 2 == 0) {
-			System.out.println(value + " is not a prime number: divisible by 2.");
+			System.out.println(value + " is not a prime number: it is already divisible by 2.");
 			return false;
-		} 
+		}
 		for (int i = 3; i <= root; i += 2) {
 			count++;
-			
+
 			if (value % i == 0) {
 				System.out.println(value + " is not a prime number.");
 				System.out.println("count:" + count);
 				return false;
-				}
 			}
 		}
 		System.out.println(value + " is a prime number.");
 		System.out.println("count:" + count);
 		return false;
 	}
-	
-	
 
-	//test it here;
+	// test it here;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//first by creating new object and instance method;
-		PrimeClass3 pc = new PrimeClass3();
-		pc.prime22233(1103);
+		// first by creating new object and instance method;
+		PrimeClass pc = new PrimeClass();
+		pc.primeTesting(1103);
 
-		//Second, using a user input;
+		// Second, using a user input;
 		System.out.println("please enter your Integer here: ");
 		Scanner scn = new Scanner(System.in);
-		int input = scn.nextInt();
 
-		PrimeClass3 pc1 = new PrimeClass3();
+		PrimeClass pc1 = new PrimeClass();
 
-		pc1.prime22233(input);
-
+		pc1.primeTesting(scn.nextInt());
 	}
 }
