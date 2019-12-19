1b48cf82ebd220873a826713d4f9ab33d5d4633b
johnmanman <57596728+johnmanman@users.noreply.github.com>
2019-12-19 18:14:01 -0500

    Create fixingTheCodeIssues.java

---
+++ from mesud/fixingTheCodeIssues.java
@@ -0,0 +1,6 @@
+
+// TODO: Fix the code issues before anything
+					
+          //Address variables were not proberly assigned, I had to assign them;
+					//one of the Address and the Student classes were not overriding the equals() and hashcode();
+					//id was given the geters and setters methods, which I had to correct it; avoided the encapsulations there);
