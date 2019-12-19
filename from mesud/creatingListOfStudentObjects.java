ae66d27a62bc9da58d9da56036363576fc70e0b4
johnmanman <57596728+johnmanman@users.noreply.github.com>
2019-12-19 17:11:29 -0500

    Create creatingListOfStudentObjects.java

---
+++ from mesud/creatingListOfStudentObjects.java
@@ -0,0 +1,29 @@
+package org.learn.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Application {
+
+
+	public static void main(String[] args) {
+
+// add the Student objects into an ArrayList named list;
+		List<Student> list = new ArrayList<Student>();
+		list.add(std);
+		list.add(std1);
+		list.add(std2);
+		list.add(std3);
+		list.add(std4);
+
+//for(Student st: list)
+
+		// When I first tried to print it(using System.out.println(list), it only gave
+		// me few
+		// hexadecimal numbers and alphabets;
+//		I then generated the toString() for the Student and the address classes. It then worked well;
+
+		System.out.println(" ========== The list of Students BEFORE sorting == works fine==");
+    }
+   }
