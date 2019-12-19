275a1104b53f3b57d920cdd2b9d92019affe5242
johnmanman <57596728+johnmanman@users.noreply.github.com>
2019-12-19 17:09:19 -0500

    Create creatingPermanintAddressObjects.java

---
+++ from mesud/creatingPermanintAddressObjects.java
@@ -0,0 +1,17 @@
+package org.learn.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Application {
+
+	public static void main(String[] args) {
+  
+  // create the permanent addresse objects;
+		Address permanentAddress = new Address(2034, "ACD str", "Greenbelt", "Maryland", 20770);
+		Address permanentAddress1 = new Address(203, "AC str", "Grblt", "Marynd", 20770);
+		Address permanentAddress2 = new Address(2050, "DMV str", "Greenbelt", "Myand", 21233);
+		Address permanentAddress3 = new Address(2020, "ZXT str", "Greenbelt", "Maryland", 24567);
+    }
+   }
