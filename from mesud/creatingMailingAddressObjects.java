c2275441c06e384462351ad1e2de892a1151d038
johnmanman <57596728+johnmanman@users.noreply.github.com>
2019-12-19 17:06:50 -0500

    Create creatingMailingAddressObjects.java

---
+++ from mesud/creatingMailingAddressObjects.java
@@ -0,0 +1,16 @@
+package org.learn.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Application {
+public static void main(String[] args) {
+// create the mailingAddress objects;
+
+		Address mailingAddress = new Address(2030, "dc str", "silverspring", "Maryland", 20770);
+		Address mailingAddress1 = new Address(2030, "dc str", "silverspring", "Maryland", 20770);
+		Address mailingAddress2 = new Address(1023, "mc str", "greenbelt", "Maryland", 21345);
+		Address mailingAddress3 = new Address(1056, "adm str", "ohio", "ohio", 20435);
+    }
+   }
